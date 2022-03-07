08056ab1f5ed9edc588cd99454202a14300df2c6
Clifton King <cliftonk@gmail.com>
2022-03-07 11:52:11 -0600

    fix get-nft-owners

--- scripts/get-nft-owners.ts
+++ scripts/get-nft-owners.ts
@@ -1,30 +1,28 @@
 import { ATXDAOUkraineNFT } from '../typechain-types/ATXDAOUkraineNFT';
-import { getContractAddress, ContractName } from '../util/contract-meta';
+import { getContractAddress } from '../util/contract-meta';
+import { BigNumber } from 'ethers';
 import { task } from 'hardhat/config';
 
-interface GetNftOwnersArgs {
-  contract: ContractName;
-}
+task(
+  'get-nft-owners',
+  'gets a list of nft owners, ordered by token id'
+).setAction(async ({}, { ethers, network }) => {
+  const contractAddress = getContractAddress('ATXDAOUkraineNFT', network.name);
+  const nft = (await ethers.getContractAt(
+    'ATXDAOUkraineNFT',
+    contractAddress
+  )) as ATXDAOUkraineNFT;
 
-task('get-nft-owners', 'gets a list of nft owners, ordered by token id')
-  .addParam<ContractName>('contract', 'contract name (e.g. ATXDAOUkraineNFT)')
-  .setAction(async ({ contract }: GetNftOwnersArgs, { ethers, network }) => {
-    const contractAddress = getContractAddress(contract, network.name);
-    const nft = (await ethers.getContractAt(
-      contract,
-      contractAddress
-    )) as ATXDAOUkraineNFT;
+  const nftData: { owner: string; tier: number; value: string }[] = [];
+  const [owners, tiers, values] = await nft.getOwners();
+  owners.forEach((owner, i) =>
+    nftData.push({
+      owner,
+      tier: tiers[i].toNumber(),
+      value: values[i].toString(),
+    })
+  );
 
-    const nftData: { owner: string; tier: number; value: number }[] = [];
-    const [owners, tiers, values] = await nft.getOwners();
-    owners.forEach((owner, i) =>
-      nftData.push({
-        owner,
-        tier: tiers[i].toNumber(),
-        value: values[i].div(1e18).toNumber(),
-      })
-    );
-
-    console.error(`${nftData.length} found!`);
-    console.log(JSON.stringify(nftData, null, 4));
-  });
+  console.error(`${nftData.length} found!`);
+  console.log(JSON.stringify(nftData, null, 4));
+});
